d3f1da462f8aa6e16610e385ec9e6f48b35b7e28
moshiroi <mqsas1337@gmail.com>
2024-11-03 02:19:39 +0000

    serde just better

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use std::{path::PathBuf, str::FromStr, time::Duration};
+use std::{env, path::PathBuf, str::FromStr, time::Duration};
 
 use eyre::OptionExt;
+use serde::Deserialize;
 use thirtyfour::prelude::*;
 use url::Url;
 use yaml_rust::{Yaml, YamlLoader};
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    let config_path = PathBuf::from_str("config.yaml")?;
-    let yaml = BookDetails::from_path(config_path)?;
+    let config_path = PathBuf::from_str("./config.yaml")?;
+    let yaml = std::fs::read_to_string(config_path)?;
+    let serde_yaml: BookDetails = serde_yaml::from_str(&yaml)?;
+    // let yaml = BookDetails::from_path(config_path)?;
 
     let caps = DesiredCapabilities::chrome();
     let driver = WebDriver::new("http://localhost:4444", caps.clone()).await?;
@@ -23,103 +26,43 @@ async fn main() -> eyre::Result<()> {
     Ok(())
 }
 
+#[derive(Deserialize, Debug)]
 pub struct BookDetails {
     book_title: String,
-    source_url: Url,
-    chapter_title: ElementSelector,
-    chapter_content: ElementSelector,
-    next_chapter_link: ElementSelector,
+    links: Links,
+    identifiers: Identifiers,
 }
 
-impl BookDetails {
-    // TODO: make generic trait?
-    pub fn from_path(path: PathBuf) -> eyre::Result<Self> {
-        let yaml_string = std::fs::read_to_string(path)?;
-        // Multi doc support, we only care about [0]
-        let yaml = YamlLoader::load_from_str(&yaml_string)?;
-        let yaml = yaml
-            .first()
-            .ok_or_eyre(
-                "Yaml file could not be parsed - are you sure it exists in the path described?",
-            )?
-            .to_owned();
-
-        println!("{:?}", yaml);
-        let book_title = yaml["book_title"]
-            .as_str()
-            .ok_or_eyre("no book_title field in config yaml")?
-            .to_string();
-
-        let source_url = Url::parse(
-            yaml["links"]["source_ul"]
-                .as_str()
-                .ok_or_eyre("no book_title field in config yaml")?,
-        )?;
-
-        let chapter_title = ElementSelector::from_yaml(&yaml["identifiers"]["title"])?;
-        let chapter_content = ElementSelector::from_yaml(&yaml["identifiers"]["content"])?;
-        let next_chapter_link = ElementSelector::from_yaml(&yaml["identifiers"]["next_chapter"])?;
-
-        Ok(Self {
-            book_title,
-            source_url,
-            chapter_title,
-            chapter_content,
-            next_chapter_link,
-        })
-    }
+#[derive(Deserialize, Debug)]
+struct Links {
+    source_url: String,
 }
 
+#[derive(Deserialize, Debug)]
+struct Identifiers {
+    next_chapter: ElementSelector,
+    content: ElementSelector,
+    title: ElementSelector,
+}
+
+#[derive(Deserialize, Debug)]
 struct ElementSelector {
+    #[serde(rename = "tag")]
     html_tag: String,
+    #[serde(rename = "type")]
     identifier_type: HtmlIdentifier,
+    #[serde(rename = "name")]
     identifier_name: String,
     attribute: Option<String>,
 }
 
-impl ElementSelector {
-    pub fn from_yaml(yaml: &Yaml) -> eyre::Result<Self> {
-        let html_tag = yaml["tag"]
-            .as_str()
-            .ok_or_eyre("yaml does not have field tag")?
-            .to_string();
-
-        let identifier_type = HtmlIdentifier::from_str(
-            yaml["type"]
-                .as_str()
-                .ok_or_eyre("yaml does not have field type")?,
-        )?;
-
-        let identifier_name = yaml["name"]
-            .as_str()
-            .ok_or_eyre("yaml does not have field tag")?
-            .to_string();
-
-        let attribute = if (yaml["attribute"].is_badvalue()) {
-            None
-        } else {
-            Some(
-                yaml["attribute"]
-                    .as_str()
-                    .ok_or_eyre("attribute tag does not exist")?
-                    .to_string(),
-            )
-        };
-
-        Ok(Self {
-            html_tag,
-            identifier_type,
-            identifier_name,
-            attribute,
-        })
-    }
-}
-
+#[derive(Deserialize, Debug)]
 enum HtmlIdentifier {
-    Class,
+    #[serde(rename = "id")]
     Id,
+    #[serde(rename = "class_")]
+    Class,
 }
-
 impl FromStr for HtmlIdentifier {
     type Err = eyre::Report;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
