5f04575cdf14c97f7ee367a0f49f56ac762ae4df
moshiroi <mqsas1337@gmail.com>
2024-11-09 21:16:27 +0000

    crawls given a source url - doesnt seem to be scraping entire chapter contents though

--- flake.nix
+++ flake.nix
@@ -14,94 +14,6 @@
           config.allowUnfree = true;
         };
 
-        # novelScraper = pkgs.stdenv.mkDerivation {
-        #   name = "novel-scraper";
-        #   src = ./.;
-
-        #   unpackPhase = ":";
-
-        #   # Specify the build inputs (include chromedriver and Python)
-        #   buildInputs = with pkgs; [
-        #     which # Add which to ensure it's available
-        #     chromedriver
-        #     python3
-        #     python3Packages.lxml
-        #     python3Packages.selenium
-        #     python3Packages.pyyaml
-        #     python3Packages.loguru
-        #     python3Packages.beautifulsoup4
-        #     python3Packages.undetected-chromedriver
-        #   ];
-
-        #   buildPhase = ''
-        #     # Do the necessary setup here
-        #     echo "Build phase complete, nothing to run here"
-        #     touch $out
-        #   '';
-
-        #   # Define the script to copy chromedriver and run the Python file
-        #   installPhase = ''
-        #     echo "Install phase complete, nothing to run here"
-        #       TMP_OUTPUT_DIR=$(mktemp -d)
-        #     # mkdir $out
-        #   '';
-
-        #   shellHook = ''
-        #     echo "Scraper is ready to run. Set OUTPUT_DIR and run with nix develop --impure"
-
-        #     if [ -z "$OUTPUT_DIR" ]; then
-        #       echo "Please set the OUTPUT_DIR environment variable to your desired output path."
-        #     else
-        #       mkdir -p "$OUTPUT_DIR"  # Create the output directory if it doesn't exist
-        #       echo "Running the script and writing output to $OUTPUT_DIR"
-        #       CHROMEDRIVER_PATH=$(which chromedriver)
-
-        #       # Create a temporary directory and copy chromedriver
-        #       TMP_CHROMEDRIVER_DIR=$(mktemp -d)
-        #       TMP_OUTPUT_DIR=$(mktemp -d)
-        #       cp $CHROMEDRIVER_PATH $TMP_CHROMEDRIVER_DIR/
-
-        #       echo "Chromedriver copied to $TMP_CHROMEDRIVER_DIR"
-
-        #       # Run the Python script
-        #       python3 ./main.py 
-        #         ./config.yaml
-        #       } $TMP_CHROMEDRIVER_PATH/chromedriver $OUTPUT_DIR
-        #     fi
-        #   '';
-
-        # };
-
-        novelScraper = pkgs.python3Packages.buildPythonPackage {
-
-          pname = "novel-scraper";
-          version = "1.0.0";
-
-          src = ./.;
-
-          # Add your Python dependencies here
-          propagatedBuildInputs = with pkgs.python3Packages; [
-            requests
-            beautifulsoup4
-            lxml
-            selenium
-            pyyaml
-            loguru
-            beautifulsoup4
-            undetected-chromedriver
-            setuptools
-          ];
-
-          # Define your script or entry point
-          installPhase = ''
-            mkdir -p $out/bin
-            cp -r . $out/bin/
-            chmod +x $out/bin/main.py  # Make your script executable
-          '';
-
-          meta = { description = "A Python scraper tool"; };
-        };
-
         rustBuildInputs  = with pkgs; [
             rustc
             rustfmt
@@ -129,9 +41,7 @@
           ] ++ pythonBuildInputs ++ rustBuildInputs;
         };
 
-        packages.default = novelScraper;
       }
-
     );
 
 }
--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,6 @@ async fn main() -> eyre::Result<()> {
     let driver = WebDriver::new("http://localhost:4444", caps.clone()).await?;
 
     let scraper = Scraper::new(config_path, driver)?;
-    scraper.driver.maximize_window().await?;
-    scraper
-        .driver
-        .goto("https://novelbin.org/novelbin/ze-tian-ji/chapter-1")
-        .await?;
-    std::thread::sleep(Duration::from_secs(2));
     scraper.scrape().await?;
     // Always explicitly close the browser.
     scraper.driver.quit().await?;
@@ -36,7 +30,7 @@ impl Scraper {
     pub fn new(config_path: PathBuf, driver: WebDriver) -> eyre::Result<Self> {
         let yaml = std::fs::read_to_string(config_path)?;
         let book_details: BookDetails = serde_yaml::from_str(&yaml)?;
-
+        tracing::info!("Succesfully parsed scraping config");
         Ok(Self {
             driver,
             book_details,
@@ -44,12 +38,33 @@ impl Scraper {
     }
 
     pub async fn scrape(&self) -> eyre::Result<()> {
+        self.driver.maximize_window().await?;
+        // Go to source url - begin crawling
+        self.driver
+            .goto(&self.book_details.links.source_url)
+            .await?;
+        // EXCEPTION: need wait for the page to load before scraping contents
+        std::thread::sleep(Duration::from_secs(1));
+        let mut next_link = self.scrape_current_chapter().await?;
+
+        while !next_link.is_empty() {
+            self.driver.goto(next_link).await?;
+            std::thread::sleep(Duration::from_secs(1));
+            next_link = self.scrape_current_chapter().await?;
+        }
+
+        Ok(())
+    }
+
+    pub async fn scrape_current_chapter(&self) -> eyre::Result<String> {
+        tracing::info!("Starting scraping");
         let title = self
             .retrieve_element(&self.book_details.identifiers.title)
             .await?
             .text()
             .await?;
 
+        tracing::info!("Retrieved chapter title");
         dbg!(title);
 
         let contents = self
@@ -58,6 +73,7 @@ impl Scraper {
             .text()
             .await?;
 
+        tracing::info!("Retrieved chapter contents");
         dbg!(contents);
 
         let next_chapter_link = self
@@ -67,9 +83,10 @@ impl Scraper {
             .await?
             .ok_or_eyre("Could not get href of next chapter link <a>")?;
 
-        dbg!(next_chapter_link);
+        tracing::info!("Retrieved next chapter link");
+        dbg!(&next_chapter_link);
 
-        Ok(())
+        Ok(next_chapter_link)
     }
 
     pub async fn retrieve_element(&self, element: &ElementSelector) -> eyre::Result<WebElement> {
