cefadb1a7046971472ab1f77b01d0d0a1bc976f6
moshiroi <mqsas1337@gmail.com>
2024-11-10 23:32:42 +0000

    remove python deps from flake

--- flake.nix
+++ flake.nix
@@ -22,23 +22,13 @@
             pkg-config
             openssl
         ];
-        pythonBuildInputs  = with pkgs; [
-            python3
-            poetry
-            python3Packages.lxml
-            python3Packages.selenium
-            python3Packages.pyyaml
-            python3Packages.loguru
-            python3Packages.beautifulsoup4
-            python3Packages.undetected-chromedriver
-        ];
 
       in with pkgs; {
         devShells.default = mkShell {
           buildInputs = [
             chromium
             chromedriver
-          ] ++ pythonBuildInputs ++ rustBuildInputs;
+          ] ++ rustBuildInputs;
         };
 
       }
